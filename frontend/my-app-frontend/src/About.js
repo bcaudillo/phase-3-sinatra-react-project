29a040450a12be95d55d803e78bf48de19907598
bcaudillo <btcaudillo@gmail.com>
2023-01-14 12:43:20 -0700

    components created

--- frontend/my-app-frontend/src/About.js
+++ frontend/my-app-frontend/src/About.js
@@ -1,5 +1,9 @@
+import React from "react";
 function About(){
-    return
+    return(
+        <div>
+        </div>
+    )
 }
+export default About;
 
-export default About
--- frontend/my-app-frontend/src/App.js
+++ frontend/my-app-frontend/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect,useState} from 'react';
+import Search from './Search';
+import About from './About';
+
 
 function App() {
+  //Edit me
+  // const [spread,setSpread]=useState([])
+  // useEffect(()=>{
+  //     fetch("http://localhost:3000/cards")
+  //     .then(r=>r.json())
+  //     .then((cardsArray)=>{
+  //         setCards(cardsArray);
+  //     })})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Beer!</h1>
+        <About/>
+
     </div>
   );
 }
