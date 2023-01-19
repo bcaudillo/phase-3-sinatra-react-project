631cca748f3cacdaaa3c581dca1aaf8fbe2c19de
bcaudillo <btcaudillo@gmail.com>
2023-01-18 22:01:50 -0700

    CRUD functionatlity working on backend and state slightly working on front

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -19,13 +19,23 @@ class ApplicationController < Sinatra::Base
     @beer.to_json
   end
 
+  patch "/cart/:id" do 
+    @cart = Cart.find(params[:id])
+    @cart.update(params)
+    @cart.to_json
+  end
+
+  delete "/cart" do
+    @cart = Cart.destroy_all
+  end
+
+
+
+
   delete "/cart/:id" do
     @cart= Cart.find(params[:id])
-    # binding.pry
     @cart.destroy
     @cart.to_json
-    # @beer = Beer.find(id)
-    # @beer.to_json
   end
   
   delete "/beerlist/:id" do
@@ -35,14 +45,6 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/cart" do 
-    # @cart3 = Cart.find_by_or_create_by(params)
-    # @cart3 = Cart.find(params[:id])
-    # if !@cart3
-    #   @carts = Cart.new(params)
-    # else
-    # @quantity = self.quantity ++
-    # @cart3.update(quantity: @quantity)
-
     @carts = Cart.new(params)
     @carts.save
     @carts.to_json
@@ -57,13 +59,13 @@ class ApplicationController < Sinatra::Base
 
   get "/cart" do
     @carts = Cart.all
-    @cart = Cart.select(:item_name).group(:item_name).having("count(*) > 1").size
-    @cart1 = Cart.group(:item_name)
-    if !@carts
-      "Cart empty"
-    else
+    # @cart = Cart.select(:item_name).group(:item_name).having("count(*) > 1").size
+    # @cart1 = Cart.group(:item_name)
+    # if !@carts
+    #   "Cart empty"
+    # else
       @carts.to_json
-    end
+ 
     # binding.pry
     # binding.pry
     # @cart.map do |item|
