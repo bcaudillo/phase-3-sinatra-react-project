111002b344868b50627b70740819c20aa6cb4d63
Brandon Caudillo <btcaudillo@gmail.com>
2023-01-24 18:24:23 -0700

    Revert "BeerStyles component created."
    This reverts commit 0d85a01ff490b9ca32329ba7f80de91a817d5ca3.

--- frontend/my-app-frontend/src/App.js
+++ frontend/my-app-frontend/src/App.js
@@ -1,3 +1,4 @@
+import logo from './logo.svg';
 import './App.css';
 import React,{useEffect,useState} from 'react';
 import {
@@ -6,67 +7,75 @@ import {
 } from 'react-router-dom'
 import NavBar from './NavBar'
 import About from './About';
+import Cart from './Cart'
 import AddBeer from './AddBeer';
 import BeerList from './BeerList';
 import { baseUrl } from './Globals';
+import { BreadcrumbSection } from 'semantic-ui-react';
 import BeerStyles from './BeerStyles';
 
 
 function App() {
+  //Edit me
   const [beers,setBeers]=useState([])
-  const [beerStyles, setBeerStyles]=useState([])
+  const [carts, setCarts]=useState([])
 
   
 
-  // useEffect(()=>{
-  //   fetch(baseUrl + "/beerlist")
-  //   .then(r=>r.json())
-  //   .then(beers => setBeers(beers));
+  useEffect(()=>{
+    fetch(baseUrl + "/beerlist")
+    .then(r=>r.json())
+    .then(beers => setBeers(beers));
    
-  //   fetch(baseUrl + "/cart")
-  //   .then(r=>r.json())
-  //   .then(beerStyles => setBeerStyles(beerStyles))
-  //   }
-  //   ,[])
-    
-  // function onUpdateCart(cart){
-  //   console.log(cart)
-  //   console.log(carts)
-  //   const ojbIndex = carts.findIndex((item=>item.id ==cart.id))
-  //   carts[ojbIndex] = cart
-  //   console.log(carts)
-  //   setCarts([...carts])
-  // }
-  // function onAddToCart(cart){
-  //   console.log(cart)
-  //   setCarts([...carts, cart])
-  // }
-  //  function onClear(){
-  //    fetch(baseUrl+ '/cart',{
-  //      method: "DELETE"
-  //     })
-  //     setCarts([])
-  //   }
+    fetch(baseUrl + "/cart")
+    .then(r=>r.json())
+    .then(carts => setCarts(carts))
+    }
+    ,[])
+      //take original cart and spread and add new cart to array 
 
-  //  function onAddBeer(beer){
-  //   console.log(beer)
-  //   setBeers([...beers, beer])
-  //  } 
-  //  console.log(beers)
-  //  function onDeleteBeerItem(index){
-  //   console.log(index)
-  //   const newArray = beers.splice(index, 1)
-  //   console.log(newArray)
+  // function onDeleteBeer(){
   //   console.log(beers)
   //   setBeers([...beers])
-  //  }
-  //  function onDeleteCartItem(index){
-  //   console.log(index)
-  //   carts.splice(index, 1)
-  //   console.log(carts)
-  //   setCarts([...carts])
-  //  }
-  //  console.log(carts)
+  // }
+  function onUpdateCart(cart){
+    console.log(cart)
+    console.log(carts)
+    const ojbIndex = carts.findIndex((item=>item.id ==cart.id))
+    carts[ojbIndex] = cart
+    console.log(carts)
+    setCarts([...carts])
+  }
+  function onAddToCart(cart){
+    console.log(cart)
+    setCarts([...carts, cart])
+  }
+   function onClear(){
+     fetch(baseUrl+ '/cart',{
+       method: "DELETE"
+      })
+      setCarts([])
+    }
+
+   function onAddBeer(beer){
+    console.log(beer)
+    setBeers([...beers, beer])
+   } 
+   console.log(beers)
+   function onDeleteBeerItem(index){
+    console.log(index)
+    const newArray = beers.splice(index, 1)
+    console.log(newArray)
+    console.log(beers)
+    setBeers([...beers])
+   }
+   function onDeleteCartItem(index){
+    console.log(index)
+    carts.splice(index, 1)
+    console.log(carts)
+    setCarts([...carts])
+   }
+   console.log(carts)
   
 
   return (
